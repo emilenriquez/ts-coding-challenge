08852f1d7177185ad32b47f013e4d89958a9a5d1
Emil Enriquez <emil.nrqz@gmail.com>
2024-03-13 11:12:16 +0800

    fix: remove error logging from console due to usage of ReactDOM.render

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(
+// Use type assertion to handle the missing createRoot type
+const root = (ReactDOM as any).createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
